f24b3bc506495a9b959a71f5950ea69abdabf3a6
Jose Caballero <jose.caballero@epicgames.com>
2025-04-28 23:48:56 +0100

    updates fab search utils to use FAB's API

--- Python/tools/fab_tools.py
+++ Python/tools/fab_tools.py
@@ -1,43 +1,154 @@
+from dataclasses import asdict, dataclass
 import logging
-import os
-import tempfile
 from pathlib import Path
+import requests
+import tempfile
 from urllib.parse import urlparse
 
-import boto3
-import requests
-from env import LOCI_API_KEY
+from Python.env import LOCI_API_KEY
+from Python.tools.utils.loci_utils import (
+    MONGO_MASTER_ASSET_COLLECTION,
+    S3_BUCKET,
+    S3_CLIENT,
+)
 from mcp.server.fastmcp import Context, FastMCP
 
 # Get logger
 logger = logging.getLogger("UnrealMCP")
 
 
-S3_CLIENT = boto3.client("s3")
 MAX_NUM_RESULTS = 5
-BUCKET = "loci-assets"
+
+
+MAX_RESULTS = 10
+FAB_SEARCH_URL = "https://fab-admin.daec.live.use1a.on.epicgames.com/i/listings/search"
+
+
+@dataclass
+class Asset:
+    uid: str
+    title: str
+    asset_s3_path: str = None
+
+
+def get_mongo_assets(key_uid: str, key_title: str, uids: list[str]) -> list[Asset]:
+    """Filter asset IDs in MongoDB.
+    Args:
+        assets: List of Asset objects to filter.
+    Returns:
+        List of asset IDs that are present in MongoDB.
+    """
+
+    def recursive_get(key: str, x: dict) -> str:
+        """Recursively get value of mongodb key defined with dot splits"""
+        keys = key.split(".")
+        for k in keys:
+            if k in x:
+                x = x[k]
+            else:
+                return None
+        return x
+
+    try:
+        mongo_query = {key_uid: {"$in": uids}, "asset_path": {"$exists": True}}
+        mongo_projection = {key_uid: 1, key_title: 1, "asset_path": 1}
+        assets = MONGO_MASTER_ASSET_COLLECTION.find(mongo_query, mongo_projection)
+        assets = [
+            Asset(
+                uid=recursive_get(key_uid, a),
+                title=recursive_get(key_title, a),
+                asset_s3_path=a["asset_path"],
+            )
+            for a in assets
+        ]
+        return assets
+
+    except Exception as e:
+        print(f"Error filtering asset IDs in MongoDB: {e}")
+        return []
 
 
 def register_fab_tools(mcp: FastMCP):
-
     @mcp.tool()
-    def search_fab_assets(ctx: Context, text_query=None):
+    def search_fab_assets(
+        ctx: Context, text_query=None, max_results: int = MAX_RESULTS
+    ):
         """Search assets in FAB.
         Args:
             ctx: The MCP context
             text_query: Query string to search in FAB
+            max_results: Maximum number of results to return
 
         Returns:
             Dict containing keys "assets" and "returned_count"
-            "assets" is a dictionary with asset IDs as keys and relevance scores as values.
+            "assets" is a list of dictionaries with keys "uid", "title", and "asset_s3_path".
             "returned_count" is the number of assets returned.
         """
-        max_num_results = MAX_NUM_RESULTS
+        try:
+            params = {
+                "listing_types": "3d-model",
+                "asset_formats": "glb",  # Needed as we only ingested these - eventually remove
+                "q": text_query,
+            }
+
+            headers = {"accept": "application/json"}
+
+            response = requests.get(url=FAB_SEARCH_URL, headers=headers, params=params)
+            results = response.json()["results"]
+            result_uids = [r["uid"] for r in results]
+            assets = get_mongo_assets(
+                key_uid="metadata.source.fab._source.uid",
+                key_title="metadata.source.fab._source.title",
+                uids=result_uids,
+            )
+
+            if len(assets) < max_results and len(results) > max_results:
+                while len(assets) < max_results:
+                    next_cursor = response.json()["cursors"]["next"]
+                    params["cursor"] = next_cursor
+                    response = requests.get(
+                        url=FAB_SEARCH_URL, headers=headers, params=params
+                    )
+                    results = response.json()["results"]
+                    if len(results) == 0:
+                        break
+
+                    result_uids = [r["uid"] for r in results]
+                    new_assets = get_mongo_assets(
+                        key_uid="metadata.source.fab._source.uid",
+                        key_title="metadata.source.fab._source.title",
+                        uids=result_uids,
+                    )
+                    assets.extend(new_assets)
+                    if len(assets) > max_results:
+                        break
+
+            assets = [asdict(a) for a in assets[:max_results]]
+            return {"assets": assets, "returned_count": len(assets)}
+
+        except Exception as e:
+            return {"error": f"FAB search failed: {str(e)}"}
+
+    @mcp.tool()
+    def search_loci_assets(
+        ctx: Context, text_query=None, max_results: int = MAX_RESULTS
+    ):
+        """Search assets in LOCI.
+        Args:
+            ctx: The MCP context
+            text_query: Query string to search
+            max_results: Maximum number of results to return
+
+        Returns:
+            Dict containing keys "assets" and "returned_count"
+            "assets" is a list of dictionaries with keys "uid", "title", and "asset_s3_path".
+            "returned_count" is the number of assets returned.
+        """
 
         try:
             url = "https://dev.loci-api.com/3d/search"
             params = {
-                "page_size": max_num_results,
+                "page_size": max_results,
                 "page_number": 1,
                 "should_translate": "false",
                 "debug": "true",
@@ -50,29 +161,31 @@ def register_fab_tools(mcp: FastMCP):
             data = {
                 "text_query": text_query,
                 "model_name": "loci",
-                "similarity_threshold": 0.85,
+                "similarity_threshold": 0.5,
             }
 
             response = requests.post(url, headers=headers, params=params, data=data)
-            hits = response.json().get("hits")
-            assets = {
-                h["asset_id"]: {
-                    "name": h["filename"].split(".")[0],
-                    "relevance": h["similarity"],
-                }
-                for h in hits
-            }
+            results = response.json().get("hits")
+            results = sorted(results, key=lambda x: x["similarity"], reverse=True)
+            results_uids = [h["asset_id"] for h in results]
+
+            assets = get_mongo_assets(
+                key_uid="source_id",
+                key_title="asset_name",
+                uids=results_uids,
+            )
+            assets = [asdict(a) for a in assets[:max_results]]
             return {"assets": assets, "returned_count": len(assets)}
 
         except Exception as e:
             return {"error": f"FAB search failed: {str(e)}"}
 
     @mcp.tool()
-    def download_fab_asset(ctx: Context, asset_id: str):
+    def download_s3_asset(ctx: Context, asset_s3_path: str):
         """Download a FAB asset.
         Args:
             ctx: The MCP context
-            asset_id: The ID of the asset to download
+            asset_s3_path: S3 URI of the asset to download
         Returns:
             Dict containing keys "status", "message", and "file_path"
             "status" indicates success or failure
@@ -80,20 +193,16 @@ def register_fab_tools(mcp: FastMCP):
             "file_path" is the local path to the downloaded asset
         """
         try:
-            asset_path = (
-                f"s3://loci-assets/dataset_Objaverse-V1/asset_{asset_id}/{asset_id}.glb"
-            )
-
             temp_dir = tempfile.mkdtemp()
             try:
                 # Parse the S3 URI
-                parsed = urlparse(asset_path)
+                parsed = urlparse(asset_s3_path)
                 s3_key = parsed.path.lstrip("/")
                 file_name = Path(s3_key).name
                 local_path = Path(temp_dir) / file_name
 
                 # Download the file
-                S3_CLIENT.download_file(BUCKET, s3_key, str(local_path))
+                S3_CLIENT.download_file(S3_BUCKET, s3_key, str(local_path))
 
             except Exception as e:
                 return {"error": f"Failed to download model: {str(e)}"}
@@ -103,7 +212,6 @@ def register_fab_tools(mcp: FastMCP):
                 "message": "Asset downloaded successfully",
                 "file_path": local_path,
             }
+
         except Exception as e:
             return {"error": f"Failed to download asset: {str(e)}"}
-
-    logger.info("FAB tools registered successfully")
---
+++ Python/tools/utils/loci_utils.py
@@ -0,0 +1,28 @@
+import json
+from aiohttp import ClientError
+import boto3
+import certifi
+from pymongo import MongoClient
+
+
+def get_secret(secret_name: str, region_name: str = "eu-west-2") -> dict[str, str]:
+    # Create a Secrets Manager client
+    session = boto3.session.Session()  # type: ignore
+    client = session.client(service_name="secretsmanager", region_name=region_name)
+
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    except ClientError as e:
+        print(f"Error retrieving secret: {e}")
+        raise e
+
+    # Decrypts secret using the associated KMS key.
+    return json.loads(get_secret_value_response["SecretString"])
+
+
+SECRETS_DICT = get_secret("loci/dev")
+MONGO_CLIENT = MongoClient(SECRETS_DICT["MONGO_DB_URL"], tlsCAFile=certifi.where())
+MONGO_MASTER_ASSET_COLLECTION = MONGO_CLIENT["main"]["assets_master"]
+
+S3_CLIENT = boto3.client("s3")
+S3_BUCKET = "loci-assets"
